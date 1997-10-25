cfde22dbb09f7c423d0b1688b388bd8afcbb0de6
Jeff Bailey <jbailey@raspberryginger.com>
1997-10-25 05:20:32 +0000

    More work on src/io-gtk.c towards building GTK interface.  Have drawing window in place.  Fix Scroll bars.  Add options to file menu.

--- src/io-gtk.c
+++ src/io-gtk.c
@@ -23,6 +23,49 @@ the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.  */
 #ifdef HAVE_LIBGTK
 #include "gtk/gtk.h"
 
+static GdkPixmap *pixmap = NULL;
+
+void
+gio_destroy (void)
+{
+	gtk_exit(0);
+}
+
+gint
+gio_expose_backing (GtkWidget *widget, GdkEventExpose *event)
+{
+	gdk_draw_pixmap(widget->window,
+			widget->style->fg_gc[GTK_WIDGET_STATE (widget)],
+			pixmap,
+			event->area.x, event->area.y,
+			event->area.x, event->area.y,
+			event->area.width, event->area.height);
+
+	return FALSE;
+}
+
+static gint
+gio_configure_backing (GtkWidget *widget, GdkEventConfigure *event)
+{
+	if (pixmap) {
+		gdk_pixmap_destroy (pixmap);
+	}
+	
+	pixmap = gdk_pixmap_new(widget->window,
+				widget->allocation.width,
+				widget->allocation.height,
+				-1);
+
+	gdk_draw_rectangle (pixmap,
+			    widget->style->white_gc,
+			    TRUE,
+			    0, 0,
+			    widget->allocation.width,
+			    widget->allocation.height);
+
+	return TRUE;
+}
+
 void
 gio_main(void)
 {
@@ -33,19 +76,23 @@ gio_main(void)
 	GtkWidget *menu_items;
 	GtkWidget *box1;
 	GtkWidget *box2;
+	GtkWidget *box3;
 	GtkWidget *scrollbar;
+	GtkWidget *drawing_area;
 	GtkObject *adjustment;
 
 	window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
 	gtk_window_set_title(GTK_WINDOW (window), GNU_PACKAGE);
 	gtk_container_border_width (GTK_CONTAINER (window), 0);
+	gtk_signal_connect (GTK_OBJECT (window), "destroy",
+			    GTK_SIGNAL_FUNC (gio_destroy), NULL);
 
 	box1 = gtk_vbox_new (FALSE, 0);
 	gtk_container_add (GTK_CONTAINER (window), box1);
 	gtk_widget_show (box1);
 
-	box2 = gtk_vbox_new (FALSE, 10);
-	gtk_container_border_width (GTK_CONTAINER (box2), 10);
+	box2 = gtk_vbox_new (FALSE, 0);
+	gtk_container_border_width (GTK_CONTAINER (box2), 0);
 	gtk_box_pack_start (GTK_BOX (box1), box2, TRUE, TRUE, 0);
 	gtk_widget_show (box2);
 
@@ -56,8 +103,23 @@ gio_main(void)
 	menu = gtk_menu_new ();
 	root_menu = gtk_menu_item_new_with_label("File");
 	gtk_widget_show (root_menu);
+	menu_items = gtk_menu_item_new_with_label("New");
+	gtk_menu_append (GTK_MENU (menu), menu_items);
+	gtk_widget_show (menu_items);
 	menu_items = gtk_menu_item_new_with_label("Open");
 	gtk_menu_append(GTK_MENU (menu), menu_items);
+	gtk_widget_show (menu_items);
+	menu_items = gtk_menu_item_new_with_label("Close");
+	gtk_menu_append(GTK_MENU (menu), menu_items);
+	gtk_widget_show (menu_items);
+	menu_items = gtk_menu_item_new_with_label("Save");
+	gtk_menu_append(GTK_MENU (menu), menu_items);
+	gtk_widget_show (menu_items);
+	menu_items = gtk_menu_item_new_with_label("Save As");
+	gtk_menu_append (GTK_MENU (menu), menu_items);
+	gtk_widget_show (menu_items);
+	menu_items = gtk_menu_item_new_with_label("Print");
+	gtk_menu_append (GTK_MENU (menu), menu_items);
 	gtk_widget_show (menu_items);
 	menu_items = gtk_menu_item_new_with_label("Exit");
 	gtk_menu_append (GTK_MENU (menu), menu_items);
@@ -90,16 +152,34 @@ gio_main(void)
 	gtk_menu_item_right_justify (GTK_MENU_ITEM (root_menu));
 	gtk_menu_bar_append (GTK_MENU_BAR (menu_bar), root_menu);
 
+	box3 = gtk_hbox_new (FALSE, 0);
+	gtk_container_border_width (GTK_CONTAINER (box3), 0);
+	gtk_box_pack_start (GTK_BOX (box2), box3, TRUE, TRUE, 0);
+	gtk_widget_show (box3);
+
+	drawing_area = gtk_drawing_area_new ();
+	gtk_drawing_area_size (GTK_DRAWING_AREA (drawing_area), 675, 350);
+	gtk_box_pack_start (GTK_BOX (box3), drawing_area, TRUE, TRUE, 0);
+	gtk_widget_show (drawing_area);
+	
+	gtk_signal_connect (GTK_OBJECT (drawing_area), "configure_event",
+			    (GtkSignalFunc) gio_configure_backing, NULL);
+	gtk_signal_connect (GTK_OBJECT (drawing_area), "expose_event",
+			    (GtkSignalFunc) gio_expose_backing, NULL);
+
+	gtk_widget_set_events (drawing_area, GDK_EXPOSURE_MASK
+			       | GDK_PROXIMITY_OUT_MASK);
+
 	adjustment = gtk_adjustment_new (0.0, 0.0, 101.0, 0.1, 1.0, 1.0);
 	scrollbar = gtk_vscrollbar_new (GTK_ADJUSTMENT (adjustment));
 	gtk_range_set_update_policy (GTK_RANGE (scrollbar), GTK_UPDATE_CONTINUOUS);
-	gtk_box_pack_start (GTK_BOX (box2), scrollbar, TRUE, TRUE, 0);
+	gtk_box_pack_start (GTK_BOX (box3), scrollbar, FALSE, FALSE, 0);
 	gtk_widget_show (scrollbar);
 
 	adjustment = gtk_adjustment_new (0.0, 0.0, 101.0, 0.1, 1.0, 1.0);
 	scrollbar = gtk_hscrollbar_new (GTK_ADJUSTMENT (adjustment));
 	gtk_range_set_update_policy (GTK_RANGE (scrollbar), GTK_UPDATE_CONTINUOUS);
-	gtk_box_pack_start (GTK_BOX (box2), scrollbar, TRUE, TRUE, 0);
+	gtk_box_pack_start (GTK_BOX (box2), scrollbar, FALSE, FALSE, 0);
 	gtk_widget_show (scrollbar);
 
 	gtk_widget_show (window);
@@ -108,4 +188,7 @@ gio_main(void)
 
 	exit (0);
 }
+
+
 #endif /* Have HAVE_LIBGTK */
+
