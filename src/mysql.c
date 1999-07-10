6fe358fb4f8917cc2bac5d0d0337ec5ae6820892
Danny Backx <danny@gnu.org>
1999-07-10 07:36:00 +0000

    Add file to talk to MySQL databases.

---
+++ src/mysql.c
@@ -0,0 +1,41 @@
+/*
+ *  $Id: mysql.c,v 1.1 1999/07/10 07:36:00 danny Exp $
+ *
+ *  This file is part of Oleo, the GNU spreadsheet.
+ *
+ *  Copyright (C) 1999 by the Free Software Foundation, Inc.
+ *  Written by Danny Backx <danny@gnu.org>.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+static char rcsid[] = "$Id: mysql.c,v 1.1 1999/07/10 07:36:00 danny Exp $";
+
+#ifdef	TEST
+
+#include <stdio.h>
+#include <mysql/mysql.h>
+
+main(int argc, char argv[])
+{
+	MYSQL	db;
+
+	if (mysql_connect(&db, "roesel", "danny", "") == NULL) {
+		fprintf(stderr, "MySQL error '%s'\n", mysql_error(&db));
+		exit(1);
+	}
+	mysql_close(&db);
+}
+#endif	/* TEST */
